fd91bf14c591f7fc22f8f655246d8c5dd1f15689
shruti panwar <Shrutipanwar312@gmail.com>
2025-09-11 19:46:19 +0530

    edit files

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ const mongoUrl ="mongodb://127.0.0.1:27017/wanderlust";
 main().then(() =>{
  console.log("connection to DB")
 }).catch((err) =>{
-    console.log(err)
+    console.log(err);
 });
 async function main() {
     await mongoose.connect(mongoUrl);
